dee6cf7fd668755d61fe2b428321f996e2730126
류성하 <poi1229@hanmail.net>
2023-05-19 09:57:37 +0900

    commit

--- final_project_front/src/store/index.js
+++ final_project_front/src/store/index.js
@@ -1,7 +1,6 @@
 import Vue from "vue";
 import Vuex from "vuex";
 // import axios from "axios";
-import axios from "axios";
 import router from "@/router";
 import createPersistedState from 'vuex-persistedstate'
 
